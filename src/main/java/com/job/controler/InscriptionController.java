b6ae9e95b79a879ed22747b1728d723dacd57712
zakaria <elhahy@gmail.com>
2017-03-22 22:45:03 +0100

    remove unless page jsp

--- src/main/java/com/job/controler/InscriptionController.java
+++ src/main/java/com/job/controler/InscriptionController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 @RequestMapping("/")
-public class AppController {
+public class InscriptionController {
 
 	@RequestMapping(value = { "/"}, method = RequestMethod.GET)
 	public String homePage(ModelMap model) {
